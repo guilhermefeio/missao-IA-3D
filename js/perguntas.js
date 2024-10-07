2f8e242fd8c1d31e16ad19a75e5f84f9d66eff19
Guigostoso <148064776+guilhermefeio@users.noreply.github.com>
2024-10-07 12:21:01 -0300

    .

--- js/perguntas.js
+++ js/perguntas.js
@@ -29,14 +29,16 @@ export const perguntas = [
                 afirmacao: [
                     "racismo enfraquece a autoestima, perpetuando sentimentos de inferioridade",
                     "afirmacao 2"
-                    ]
+                    ],
+                    proxima: 1,
             },
             {
                 texto: "contribui para a exclusão social e marginalização de grupos minoritarios ",
                 afirmacao: [
                     "reforça a desigualdade e estereótipos discriminatórios",
                     "afirmacao 2"
-                    ]
+                    ],
+                    proxima: 1,
             }
         ]
     },
@@ -48,14 +50,16 @@ export const perguntas = [
                 afirmacao: [
                     "você realmente já ouviu  falar o que é o racismo?  A sua história e quantas milhares de pessoa foram prejudicadas e mortas? ",
                     "afirmacao 2"
-                    ]
+                    ],
+                    proxima: 1,
             },
             {
                 texto:"Não, sou contra o movimento racista",
                 afirmacao: [
                     "que legal! concordamos que o racismo não é uma coisa boa ",
                     "afirmacao 2"
-                    ]
+                    ],
+                    proxima: 1,
             }
             
         ]
